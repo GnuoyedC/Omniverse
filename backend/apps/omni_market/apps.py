5f98db314fec2f3c25d60a9e59091e2c5f862dac
GnuoyedC <coltendeyoung@proton.me>
2023-12-03 14:50:42 -0400

    Added the proper import path to omni_market/apps.py

--- backend/apps/omni_market/apps.py
+++ backend/apps/omni_market/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class OmniMarketConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "omni_market"
+    name = "backend.apps.omni_market"
