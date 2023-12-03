71e1e6747c3968716f72f39facab9c1e94a70e03
GnuoyedC <coltendeyoung@proton.me>
2023-12-03 14:47:07 -0400

    added the proper import path for the application so the project will be able to/ properly reference it.

--- backend/apps/omni_catalog/apps.py
+++ backend/apps/omni_catalog/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class OmniCatalogConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "omni_catalog"
+    name = "backend.apps.omni_catalog"
