382d0827a5847e29c0c0d201a03721e4c9877a6e
GnuoyedC <coltendeyoung@proton.me>
2023-12-04 15:14:00 -0400

    Modified: apps.py and settings.py - Had to remove 'backend' from the app names, and adjust the settings.py file to reflect these changes.

--- backend/apps/omni_catalog/apps.py
+++ backend/apps/omni_catalog/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class OmniCatalogConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "backend.apps.omni_catalog"
+    name = "apps.omni_catalog"
