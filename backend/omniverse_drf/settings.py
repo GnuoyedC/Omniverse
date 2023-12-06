a9e0480a91c626e134dbe4bfd23fdb7552b4b4e7
GnuoyedC <coltendeyoung@proton.me>
2023-12-06 16:11:05 -0400

    Modified: settings.py - Added a variable to expose the private key stored in the .env.marvelapi file.

--- backend/omniverse_drf/settings.py
+++ backend/omniverse_drf/settings.py
@@ -36,8 +36,8 @@ DBCONFIG = dotenv_values(ENVDB_PATH)
 # REF2: https://developer.marvel.com/docs
 MARVELCONFIG = dotenv_values(ENVMARVELAPI_PATH)
 MARVEL_API_KEY = MARVELCONFIG["PUBLIC_KEY"]
+MARVEL_PVT_KEY = MARVELCONFIG["PRIVATE_KEY"]
 MARVEL_API_ENDPOINT = MARVELCONFIG["ENDPOINT_URL"]
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
