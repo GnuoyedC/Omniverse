b267deb9652c87a452bc8a39f0a3341e4754c72f
GnuoyedC <coltendeyoung@proton.me>
2023-12-16 15:47:22 -0400

    Did... pretty much nothing.

--- backend/utils/api_db_handler.py
+++ backend/utils/api_db_handler.py
@@ -1,9 +1,10 @@
 from marvel_api_handler import MarvelAPI
 from apps.omni_catalog.models import Omnibus
 
-class APIDBHandler:
+class APIDBHandler():
     @staticmethod
     def test():
         print(Omnibus)
+
 if __name__ == '__main__':
     APIDBHandler.test()
